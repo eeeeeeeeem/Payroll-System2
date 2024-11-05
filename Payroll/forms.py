c39be3f314fe3edd1dad5891774b12667ebd3533
Penguin <vanesscapuras@gmail.com>
2024-11-05 19:50:30 +0800

    Added user settings

--- Payroll/forms.py
+++ Payroll/forms.py
@@ -1,7 +1,15 @@
 from django import forms
-from .models import Post
+from .models import Post, User
+
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['content', 'image']
+
+
+
+class UserSettingsForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name', 'date_of_birth', 'gender', 'address', 'cityId', 'employement_start', 'job_title_id', 'profile_picture']
